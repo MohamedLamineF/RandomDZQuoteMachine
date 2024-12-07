745be9e9928351749fb026df5503ad6282c75e65
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-07 16:07:05 +0100

    fix bsky button

--- src/components/QuoteContainer.tsx
+++ src/components/QuoteContainer.tsx
@@ -24,7 +24,8 @@ const QuoteContainer: React.FC<QuoteContainerProps> = ({
   };
   const blueSkyQuote = () => {
     const skeetText = encodeURIComponent(`${quote.content} ~${quote.author}`);
-    return `https://bluesky.com/intent/skeet?text=${skeetText}`;
+    return `https://bsky.app/intent/compose?text=${skeetText}`;
+    
   };
 
   const buttonClass = `flex items-center gap-2 px-4 py-1 bg-white/10 hover:bg-white/20 text-white px-4 py-2 rounded-lg flex items-center transition-all duration-300 border border-white/40`;
